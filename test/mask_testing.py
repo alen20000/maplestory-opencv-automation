24039d34729f7fbefd531b11c6f8295d496d5dc0
cliff <a0979326987@gmail.com>
2026-08-07 21:28:42 +0800

    misc:test script

--- test/mask_testing.py
+++ test/mask_testing.py
@@ -2,18 +2,17 @@ import cv2
 from pathlib import  Path
 import numpy as np
 root_dir = Path(__file__).resolve().parent.parent
-test_img = str(root_dir / "img/monsters/Dark_Stump.png")
+img_path = str(root_dir / "img/monsters/Dark_Stump.png")
 
-img = cv2.imread(test_img,cv2.COLOR_BGR2GRAY)
-# mask = cv2.imread("mask.png")
-cv2.imshow("test",img)
+img_bgr = cv2.imread(img_path,cv2.IMREAD_COLOR)
+img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+lower= np.array([35, 43, 46])  # 綠色下限
+upper = np.array([77, 255, 255])  # 綠色上限
+mask = cv2.inRange(img_hsv, lower, upper)
+mask = cv2.bitwise_not(mask)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower_green = np.array([35, 43, 46])  # 綠色下限
-upper_green = np.array([77, 255, 255])  # 綠色上限
-green_mask = cv2.inRange(hsv, lower_green, upper_green)
 
-cv2.imshow("test",green_mask)
+cv2.imshow("test",mask)
 
 
 
