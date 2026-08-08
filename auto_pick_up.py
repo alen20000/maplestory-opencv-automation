6f114bf5b0498bfdbf1127398b819c36f93de82d
cliff <a0979326987@gmail.com>
2026-08-08 15:35:22 +0800

    refect:refect a script

--- auto_pick_up.py
+++ auto_pick_up.py
@@ -5,59 +5,58 @@ import time
 import os
 import win32gui
 """
-自動撿拾
+一個簡單的自動撿拾腳本
 
 """
+AUTO_PICK_DELAY = 0.3
+GAME_TITLE = "新楓之谷：經典版"
+PICK_UP_KEY = "z"
 
 class Bat:
     def __init__(self):
-        #config
-        self.game_title = "新楓之谷：經典版"
-        self.auto_pick_delay = 0.3
         #初始化
         interception.auto_capture_devices(keyboard=True, mouse=False)
         self.enable_pick = False
         self.MapleStory_hhwnd = self.is_MapleStory_window()
 
-
-
-        
     def run(self):
         print('='* 6)
+        print("F2:開/關自動撿拾/nF3:退出程式")
         #綁定事件
-        keyboard.add_hotkey('f2',self.swithch_pick_model)
+        keyboard.add_hotkey('f2',self.able_pick_up)
         keyboard.add_hotkey('f3',self.exit_process)
         
 
-        #keeping run
+        #states
         keyboard.wait()
 
     def is_MapleStory_window(self):
-        print(self.game_title)
         try:
-            MapleStory_hhwnd = win32gui.FindWindow(None,self.game_title) 
-            print(f"找到視窗{self.game_title}，窗柄{MapleStory_hhwnd}")
+            MapleStory_hhwnd = win32gui.FindWindow(None,GAME_TITLE) 
+            print(f"找到視窗{GAME_TITLE}，窗柄{MapleStory_hhwnd}")
             return MapleStory_hhwnd
         except:
-            print(f"沒找到視窗{self.game_title}")
+            print(f"沒找到視窗{GAME_TITLE}")
             return False
 
-    def swithch_pick_model(self):
-
+    def able_pick_up(self):
+        '''
+        開啟撿拾模式
+        '''
         if self.enable_pick is False:
             print('開啟拾取')
             self.enable_pick = True
-            threading.Thread(target=self.auto_pick_up, daemon=True).start()
+            threading.Thread(target=self._pick_up_action, daemon=True).start()
 
         else:
             print('退出拾取')
             self.enable_pick = False
 
-    def auto_pick_up(self):
+    def _pick_up_action(self):
         while self.enable_pick is True:
             if win32gui.GetForegroundWindow() == self.MapleStory_hhwnd:
-                interception.press("z")
-            time.sleep(self.auto_pick_delay)
+                interception.press(PICK_UP_KEY)
+            time.sleep(AUTO_PICK_DELAY)
 
     def exit_process(self):
         print("退出結束")
