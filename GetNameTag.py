1d6148ed64d83f5e56353fb8eb4c051fd088b19a
cliff <a0979326987@gmail.com>
2026-08-09 21:43:33 +0800

    docs:update it

--- GetNameTag.py
+++ GetNameTag.py
@@ -1,8 +1,12 @@
 import tools.get_nametag
+import logging      
 '''
 暫時使用。 
 啟動模塊，抓取角色標籤，為用作名稱標籤以利匹配座標。
 '''
 if __name__ == "__main__":
-    run = tools.get_nametag. GetRoleImg()
-    run.run()
+    try:
+        run = tools.get_nametag.GetRoleImg()
+        run.run()
+    except Exception as e:
+        logging.exception(f"未預期的例外錯誤: {e}")
