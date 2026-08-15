cf70651595e37d0b6fbcd4dcbd8d603c58615bbb
cliff <a0979326987@gmail.com>
2026-08-15 12:22:03 +0800

    WIP:用匹配抓小地圖座標方向，失敗了；應該寫死top_left點，反正都一樣

--- src/engine/MinimapDetector.py
+++ src/engine/MinimapDetector.py
@@ -4,9 +4,12 @@ import logging
 import numpy as np
 '''
 人物黃點: RGB 255,255,136  HSV 是： [ 30 119 255]
+人物黃點: RGB 255,255,0  HSV 是： [ 30 255 255]
+
+learning: 想法錯了 應該不用全圖匹配換 roi匹配然後取得小地圖位置 ； 應該直接鎖死小地圖的top left  好像每張小弟圖的 topl left point 都是一個位置
 
 '''
-minimap_detect_range = (5,20,187,184)
+minimap_detect_range = (0,20,187,184)
 
 class MinimapDetector:
     def __init__(self):
@@ -14,8 +17,10 @@ class MinimapDetector:
         self.minimap_name = None
         self.minimap_template = None
         self.current_frame_bgr =None
+        
         self.map_tl = None
         self.map_br = None
+        self.crop_frame_bgr = None
         #load config
         self._load_minimap_config()
 
@@ -35,17 +40,24 @@ class MinimapDetector:
 
         #沒有minimap座標在抓
         if self.map_tl == None:
-            result = self._match_minimap(frame_bgr)
+            self.crop_frame_bgr = self._match_minimap(frame_bgr)
 
-        player_loc = self._detect_player_loc(frame_bgr)
+        if self.crop_frame_bgr:
+            player_loc = self._detect_player_loc(self.crop_frame_bgr)
+            x1 , y1 = player_loc
+            x2 , y2 = x1+10,y2+10
+            cv2.rectangle(self.current_frame_bgr,(x1,y1),(x2,y2),(100,255,100),3)
 
-        # self._draw_minimap()
-        # self._draw_match_map(result)
-        pass
+
+
+
 
     def _match_minimap(self, frame):
         """
         先用原圖去補獲真實座標，再把偵測範圍縮到固定區域做下一步處理
+
+        Returns:
+            裁切過後的frame | None
         """
 
         # print("--- DEBUG INFO ---")
@@ -53,17 +65,27 @@ class MinimapDetector:
         # print("template type:", type(self.minimap_template), "shape:", getattr(self.minimap_template, 'shape', None), "dtype:", getattr(self.minimap_template, 'dtype', None))
         # print("--------------------")
 
+        x1, y1 ,x2 , y2 = minimap_detect_range
 
-        result =  cv2.matchTemplate(frame,self.minimap_template,cv2.TM_CCOEFF_NORMED) 
+        roi_crop_frame = frame[x1:x2,y1:y2]
+
+        result =  cv2.matchTemplate(roi_crop_frame,self.minimap_template,cv2.TM_CCOEFF_NORMED) 
         _, max_val, _, max_loc = cv2.minMaxLoc(result)
 
-        if max_val > 0.8:
+        if max_val > 0.5:
             x1,y1 = max_loc[0],max_loc[1]
             x2,y2 = x1+self.minimap_template.shape[1],y1+self.minimap_template.shape[0]
             #取地圖ROI範圍
             self.map_tl = (x1,y1)
             self.map_br = (x2,y2)
 
+            crop_frame = frame[y1:y2,x1:x2]
+            return crop_frame 
+        else:
+            print(f"測試:沒找到人地圖{self.minimap_template}")
+
+
+        return None
 
 
     def _detect_player_loc(self,frame_bgr):
@@ -75,9 +97,8 @@ class MinimapDetector:
             return None
 
         frame_hsv = cv2.cvtColor(frame_bgr,cv2.COLOR_BGR2HSV)
-
-        lower_player_color = np.array([20, 60, 180])
-        upper_player_color = np.array([40, 180, 255])
+        lower_player_color = np.array([20, 100, 180])
+        upper_player_color = np.array([40, 255, 255])
 
         mask = cv2.inRange(frame_hsv,lower_player_color,upper_player_color)
         #抓住符合顏色。 這邊還有 cv2.findContours搭配cv2.RETR_EXTERNAL與cv2.CHAIN_APPROX_SIMPLE 抓輪廓的寫法，可以嘗試(可選)
@@ -102,7 +123,6 @@ class MinimapDetector:
         '''
         x1,y1,x2,y2 = minimap_detect_range
         cv2.rectangle(self.current_frame_bgr,(x1,y1),(x2,y2),(100,100,100),3)
-
         cv2.imshow("TEST", self.current_frame_bgr)
         cv2.waitKey(0)
 
