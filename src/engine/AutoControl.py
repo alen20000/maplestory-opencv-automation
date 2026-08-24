26db1ae91855ecfadb7dda3bc7ec81d51ee47942
cliff <a0979326987@gmail.com>
2026-08-24 13:28:42 +0800

    wip:狀態:COMBAT 調適完成

--- src/engine/AutoControl.py
+++ src/engine/AutoControl.py
@@ -4,6 +4,7 @@ from config.config_loader import config
 from typing import Optional
 from src.engine.game_state import GameState
 from src.engine.HealthManager import HealthManager
+from src.engine.fsm.BotState import BotState
 import time
 import random
 from pathlib import Path
@@ -19,16 +20,18 @@ from enum import Enum, auto
 '''
 class AutoControl:
     def __init__(self):
+
         #---[實例化]
         self.health_manager = HealthManager()
-        # Search Config & Constants
-        self.buffer = config.get("auto_control_config.buffer", 0) # <-- 邊界距離緩衝(平台的邊界距離+緩衝距離)
+        self.bot_state = BotState(self) # 把自己(AutoControl)交給狀態機當 owner，讓狀態機能反過來呼叫函式或共用資料
 
+        #---[外部參數&設定]
+        self.buffer = config.get("auto_control_config.buffer", 0) # <-- 邊界距離緩衝(平台的邊界距離+緩衝距離)
         self.verti_move_threshold = config.get("auto_control_config.verti_move_threshold",10)
-        # Data Containers
-        self.health_setting = {}
-        self.mp_setting = {}
-        self.search_direction = random.choice(["LEFT", "RIGHT"])
+
+        #---[內部參數&設定]
+        self.search_direction = random.choice(["LEFT", "RIGHT"]) #<-- 巡邏方向;第一次初始化隨機方向
+
         #---[座標用容器]
         self.recored_data = []#<-- 所有行為點的容器
         self.platforms = [] #<-- 所有平台
@@ -38,9 +41,7 @@ class AutoControl:
         self.mini_player_loc = None #<-- 當前人物位置(小地圖)
         self.last_player_loc = None #<-- 上次人物位置(小地圖)
         self.current_verti_target = None #<-- 當前垂直通道目標方向
-        #---[道具用容器]
-        self.mp_sorted_levels = [] #<-- 藍水等級排序
-        self.hp_sorted_levels =[] #<-- 紅水等級排序
+
         #Timer
         self.patrol_start_time = None #巡邏開始時間
 
@@ -54,16 +55,15 @@ class AutoControl:
         # Parameters
         self.player_attack_range = config.get("player_setting.auto_control_config.attack_range")
 
-        #Flags
-        self.patrol_active = True  #巡邏
-        self.battle_active = True  #戰鬥
-        #Toggle/
-        self.enable_searching_mob = config.get("auto_control_config.search_interval", False) # <-- 搜尋怪物功能(開//關)
+
     #=================
     # 載入設定
     #=================
 
     def _load_map_data(self):
+        '''
+        載入地圖模板、預設座標
+        '''
         try:
             map_name = config.get("quickly_choice_map")
             self.mini_map = Path(config.get(f"mini_map.{map_name}"))
@@ -74,164 +74,41 @@ class AutoControl:
                 with open(yaml_path, "r") as f:
                     self.recored_data = yaml.safe_load(f)
 
+                # 解析平台和垂直通道
                 self.platforms = self._find_platform()
                 self.vertical_passage = self._find_vertical_passage()
-
                 logging.info(f"{map_name}地圖載入完成，平台數量:{len(self.platforms)},垂直通道數量:{len(self.vertical_passage)}")
                 print(f"{map_name}地圖載入完成\n平台數量:{len(self.platforms)}\n垂直通道數量:{len(self.vertical_passage)}")
         except Exception as e:
             logging.error(f"載入地圖失敗{e}")
-
     #=================
-    # 主要邏輯
+    # 功能:地圖解析
     #=================
-    def run(self,state: GameState) -> tuple[Optional[str], Optional[dict]]:
-        '''
-        Args:
-            state (GameState): 包含當前角色位置、血量、ROI 範圍及怪物清單的資料容器。
-        '''
-        #===========
-        # 每輪資訊更新
-        #===========
-        
-        if state.mini_player_loc:   #角色座標更新
-            self.mini_player_loc = state.mini_player_loc
-
-        current_time =time.time()   #時間標籤
-
-        #===========
-        # 管理模組: 健康模組
-        #===========
-        self.health_manager.run(state.player_hp,state.player_mp,current_time)
-
-
-
-        # 2. 執行戰鬥
-        if self.battle_active :
-            if self.mini_player_loc and self.enable_searching_mob :
-                if platform:= self._handle_combat_logic(state):
-                    return platform
-
-        # 3. 執行巡邏
-        if self.current_platform and self.enable_searching_mob :
-            # 初始化"巡邏狀態"
-            if self.patrol_start_time is None:
-                self.patrol_start_time = current_time
-                # self.patrol_state = True
-
-            # 判斷:幾秒找不到怪，進入找通道
-            if   current_time - self.patrol_start_time > self.verti_move_threshold:
-                action = self._enter_verti()
-            elif self.patrol_active:
-                # 開始巡弋找怪
-
-                result = self._enable_player_patrol()
-                return result
-
-        # 4.角色脫困
-        if self.current_platform is None and self.current_vertical_passage is None:
-
-            # 平台在更新一下，也許掉到別的平台
-            self.current_platform = self._check_current_platform()
-
-            if self.current_platform is  None:
-                return self.player_unstuck()
-
-        # 5.其他路徑移動(垂直移動)
-        if self.mini_player_loc:
-            try:
-
-                # 判斷:人物是否在垂直通道內
-                self.current_vertical_passage = self._check_vertical_passage()
-
-                # 觸發:不在垂直通道範圍
-                if self.current_vertical_passage is None:
-                    # 判斷:方向存在
-                    if self.current_verti_target is not None:
-                        self.current_verti_target = None
-                        self.is_climbing_state = False
-                        self.is_finding_rope_state = False
-                        self.patrol_active = True
-                        self.is_combat_state = True
-                    #自動去最近的垂直通道
-                    index = self._find_nearest_verti_passage()
-                    print(f"\r自動去最近的{index +1}號垂直通道",end="", flush=True)
-                    return  self._move_to_verti_passage(index)
-
-                # 觸發:人物在垂直通道範圍 
-                if self.current_vertical_passage is not None:
-                    print("\r開始爬抓取繩子",end="", flush=True)
-                    self.pervious_time = current_time
-
-                    # 重置狀態屬性:不可戰鬥
-                    self.is_combat_state = False
-
-                    # 開始垂直移動
-                    result = self._verti_movement()
-                    if result is not None:
-                        return result
-                else:
-                    # 假如，爬繩子，則重置狀態屬性
-                    
-                    if self.is_climbing_state or self.is_finding_rope_state:
-                        self.patrol_active = True
-                    self.is_climbing_state = False
-                    self.is_finding_rope_state = False
-                    
-            except Exception as e:
-                logging.error(f"垂直通道判斷失敗{e}")
-
-        # 重置爬行方向的狀態
-        if self.current_verti_target is None:
-            self.current_verti_target = None
-        # 如果人物脫離垂直通道，則允許戰鬥
-        if self.current_vertical_passage is None:
-            self.is_combat_state = True
-
-
-
-        return None,None
-
-    def _handle_combat_logic(self, state: GameState) -> tuple[Optional[str], Optional[dict]]:
+    def _find_platform(self):
         """
-        模塊:整合平台內判斷、打怪
+        功能:
+            分析路徑設定，拆出每個平台、平台範圍。
         """
-            #觸發:有人物座標時
-        if self.mini_player_loc:
-            try:
-                # 平台判斷
-                self.current_platform = self._check_current_platform()
-            except Exception as e:
-                logging.error(f"平台判斷失敗{e}")
+        platforms = []
+        i= 0
+        while i < len(self.recored_data)-1:
+            current = self.recored_data[i]
+            next_item = self.recored_data[i + 1]
 
-        #觸發:人物在平台內時
-        if self.current_platform is not None:
-            #有怪則找怪
-            if state.mobs:
-                
-                # 觸發:有怪物就重置巡邏計時與狀態
-                self.patrol_start_time = None
-                # self.patrol_state = True
+            if current["action"] =='walk' and next_item["action"] == 'walk':
 
-                result = self._fk_that_mob(state)
-                return result
-            
+                top = current["loc"][1] - 5   #向上偏移:5
+                bottom = current["loc"][1]  + 5  #向下偏移:3
+                left = min(current["loc"][0], next_item["loc"][0])
+                right = max(current["loc"][0], next_item["loc"][0]) 
 
-    def player_unstuck(self):
-        '''
-        角色脫困
-        '''
+                platforms.append({"t_l":(left,top),"b_r":(right,bottom)})
+                i += 2
+            else:
+                i += 1 
 
-        #去最近平台
-        result = self._find_nearest_platform()
-        if result is not None:
-            return self._move_to_platform(result)
-        else:
-            return None
-
-    #=================
-    # 邏輯塊: 垂直通道
-    #=================
+        return platforms
+    
     def _find_vertical_passage(self):
         '''
         找出垂直通道(繩子)，並製作出垂直通道範圍
@@ -260,11 +137,66 @@ class AutoControl:
                 i += 1
 
         return passage
+
+    #=================
+    # 主要邏輯
+    #=================
+    def run(self,state: GameState) -> tuple[Optional[str], Optional[dict]]:
+        '''
+        Args:
+            state (GameState): 包含當前角色位置、血量、ROI 範圍及怪物清單的資料容器。
+        '''
+        #===========
+        # 每輪資訊更新
+        #===========
+        
+        if state.mini_player_loc:   #角色座標更新
+            self.mini_player_loc = state.mini_player_loc
+        current_time =time.time()   #時間標籤
+
+        #依賴檢查
+        if self.mini_player_loc is None or self.platforms is None:
+            logging.warning("缺失自動化必要信息，跳過此幀")
+            return None, None
+
+        if self.current_platform is None:
+            self.current_platform = self._check_current_platform() #判斷人在哪一個平台
+        #===========
+        # 管理模組: 健康模組
+        #===========
+        self.health_manager.run(state.player_hp,state.player_mp,current_time)
+
+        action, params = self.bot_state.handle(state)   
+        return action, params 
+
+    '''
+    功能塊: 感知函式 
+    '''
+    def _check_current_platform(self):
+        """
+        功能:
+            根據玩家目前的座標
+            判斷人在哪一個平台內
+        """
+        if not self.mini_player_loc or not self.platforms:
+            return None
+
+        px, py = self.mini_player_loc 
+
+        for index, plat in enumerate(self.platforms):
+            left, top = plat["t_l"]     
+            right, bottom = plat["b_r"] 
+
+            if left <= px <= right and top <= py <= bottom:
+                return index  
+
+        return None
     
     def _check_vertical_passage(self) -> Optional[int]:
         '''
-        判斷玩家在哪個垂直通道內
-
+        功能:
+            根據玩家目前的座標
+            判斷玩家在哪個垂直通道內
         returns: 
             傳回垂直通道的index|None
         '''
@@ -278,11 +210,29 @@ class AutoControl:
             right, bottom = vert["b_r"] 
 
             if left <= px <= right and top <= py <= bottom:
- 
+
                 return index  
 
         return None
 
+
+
+
+    def player_unstuck(self):
+        '''
+        角色脫困
+        '''
+
+        #去最近平台
+        result = self._find_nearest_platform()
+        if result is not None:
+            return self._move_to_platform(result)
+        else:
+            return None
+
+    #=================
+    # 邏輯塊: 垂直通道
+    #=================
 
     def _verti_movement(self):
         """
@@ -378,124 +328,17 @@ class AutoControl:
         self.is_climbing_state = False
         self.patrol_active = True
         self.battle_active = True
-    #=================
-    # 邏輯塊: 水平平台
-    #=================
-    def _find_platform(self):
-        """
-        找出平台
-        用滑動窗口直接從list內抓出平台，加上偏移量，製作出平台範圍
-        """
-
-        platforms = []
-
-        #平台誤差範圍
-
-        i= 0
-        while i < len(self.recored_data)-1:
-            current = self.recored_data[i]
-            next_item = self.recored_data[i + 1]
 
 
-            if current["action"] =='walk' and next_item["action"] == 'walk':
-
-                top = current["loc"][1] - 5   #向上偏移:5
-                bottom = current["loc"][1]  + 3  #向下偏移:3
-
-                left = min(current["loc"][0], next_item["loc"][0])
-                right = max(current["loc"][0], next_item["loc"][0]) 
-
-                platforms.append({"t_l":(left,top),"b_r":(right,bottom)})
-                i += 2
-            else:
-                i += 1 
-
-        return platforms
-
-    def _check_current_platform(self):
-        """
-        根據玩家目前的座標，判斷人在哪一個平台內
-        """
-        if not self.mini_player_loc or not self.platforms:
-            return None
-
-        px, py = self.mini_player_loc 
-
-        for index, plat in enumerate(self.platforms):
-            left, top = plat["t_l"]     
-            right, bottom = plat["b_r"] 
-
-            if left <= px <= right and top <= py <= bottom:
-                return index  
-
-        return None
 
 
-    #=================
-    # 邏輯塊: 打怪物
-    #=================
-    def _fk_that_mob(self,state):
-        '''
-        功能:在尋怪範圍內攻擊怪物
-        '''
-        if not state.player_center_loc:
-            return None, None
-        px, _ = state.player_center_loc
 
-        # 計算距離/方向/目標/回傳 states module 結果
-        best_target = None
-        min_distance = float('inf')
 
-        #從無限遠開始判斷
-        for mob , mob_detail in state.mobs or []:
-            for detailed in mob_detail:
-                #計算怪物的絕對座標，
-                mx = detailed["top_left"][0] + state.roi_BBOX.x1
-
-                #絕對值求與玩家間的距離
-                distance = abs(px - mx)
-                if distance < min_distance:
-                    min_distance = distance
-                    #左右判斷
-                    direction = "RIGHT" if px < mx else "LEFT"
-                    best_target = {"name": mob, "distance": distance, "direction": direction}
-        #攻擊距離判斷在這行
-        if best_target and best_target['distance'] <= self.player_attack_range:
-            #debug
-            # print(f"目標 [{best_target['name']}] 在攻擊範圍內 距離: {best_target['distance']} 方向: {best_target['direction']}")
-            return "ATTACK" , best_target
-
-        return None
 
     #=================
     # 邏輯塊: 人物移動
     #=================
-    def _enable_player_patrol(self)-> tuple[Optional[str], Optional[dict]]:
-        '''
-        功能:平台範圍內左右巡邏
-        '''
 
-        px, py = self.mini_player_loc
-
-        #要 -1 因為求平台時多加了
-        plat_index = self.current_platform - 1
-        current_plat = self.platforms[plat_index]
-        
-        left_bound = current_plat["t_l"][0]   # 平台的左極限 X
-        right_bound = current_plat["b_r"][0]  # 平台的右極限 X
-
-        if px <= left_bound + self.buffer:
-            self.search_direction = "RIGHT"   # 走到底右轉
-
-            return self._pack_action("MOVE", direction="RIGHT")
-
-        elif px >= right_bound - self.buffer:
-            self.search_direction = "LEFT"    # 走到底左轉
-
-            return self._pack_action("MOVE", direction="LEFT")
-        #這句不能改，否則人物會罰站
-        else:
-            return self._pack_action("MOVE", direction=self.search_direction)
 
     def _find_nearest_platform(self):
         """
@@ -677,3 +520,78 @@ class AutoControl:
             {"label": "platform", "color": (193,255,193),   "boxes": [(p["t_l"], p["b_r"]) for p in self.platforms]},
             {"label": "vertical_passage", "color": (0,100,0), "boxes": [(v["t_l"], v["b_r"]) for v in self.vertical_passage]},
         ]
+
+
+    #=================
+    # 分類: 已與狀態機掛勾執行函式
+    #=================
+    def _enable_player_patrol(self)-> tuple[Optional[str], Optional[dict]]:
+        '''
+        功能:
+            人物開始巡邏
+        效果:
+            在平台範圍內左右移動
+        需求:
+            - self.mini_player_loc : 人物座標
+            - self.current_platform : 當前平台index
+        '''
+        # 依賴檢查：無平台資訊就拋棄此幀
+        if self.current_platform is None :
+            return None,None
+        print(f"狀態:巡邏中...平台:{self.current_platform+1}")
+        px, _ = self.mini_player_loc
+
+        plat_index = self.current_platform
+        current_plat = self.platforms[plat_index]
+        
+        left_bound = current_plat["t_l"][0]   # 平台的左極限 X
+        right_bound = current_plat["b_r"][0]  # 平台的右極限 X
+
+        if px <= left_bound + self.buffer:
+            self.search_direction = "RIGHT"   # 走到底右轉
+
+            return self._pack_action("MOVE", direction="RIGHT")
+
+        elif px >= right_bound - self.buffer:
+            self.search_direction = "LEFT"    # 走到底左轉
+
+            return self._pack_action("MOVE", direction="LEFT")
+        #這句不能改，否則人物會罰站
+        else:
+            return self._pack_action("MOVE", direction=self.search_direction)
+
+    def _fk_that_mob(self,state):
+        '''
+        功能:
+            計算與怪物距離，判斷目標並攻擊
+        '''
+        # 依賴檢查：無平台資訊就拋棄此幀
+        if not state.player_center_loc:
+            return None, None
+        
+        px, _ = state.player_center_loc
+
+        # -- 計算最近的怪物
+        best_target = None
+        min_distance = float('inf')
+
+        #從無限遠開始判斷
+        for mob , mob_detail in state.mobs or []:
+            for detailed in mob_detail:
+                #計算怪物的絕對座標，
+                mx = detailed["top_left"][0] + state.roi_BBOX.x1
+
+                #絕對值求與玩家間的距離
+                distance = abs(px - mx)
+                if distance < min_distance:
+                    min_distance = distance
+                    #左右判斷
+                    direction = "RIGHT" if px < mx else "LEFT"
+                    best_target = {"name": mob, "distance": distance, "direction": direction}
+        #攻擊距離判斷在這行
+        if best_target and best_target['distance'] <= self.player_attack_range:
+            #debug
+            # print(f"目標 [{best_target['name']}] 在攻擊範圍內 距離: {best_target['distance']} 方向: {best_target['direction']}")
+            return "ATTACK" , best_target
+        print("沒有目標在攻擊範圍內")
+        return None, None 
--- src/engine/fsm/BotState.py
+++ src/engine/fsm/BotState.py
@@ -1,18 +1,90 @@
-import time
-import logging
+
 from config.config_loader import config
-from typing import Optional
-from src.engine.game_state import GameState
+from abc import ABC, abstractmethod
 import time
-import random
-from pathlib import Path
-import yaml
-import time
-from enum import Enum, auto
+
+class State(ABC):
+    #強制約束方法
+    @abstractmethod
+    def handle(self, context, state_data):
+        pass
 
 
-class BotState(Enum):
-    IDLE = auto()
-    MOVING = auto()
-    HUNTING = auto()
-    FIGHTING = auto()
+class PatrolState(State):
+
+    TIMEOUT = 3 # <-- 平台巡邏超過幾秒，結束巡邏，進入下一個狀態
+
+    def __init__(self):
+        self.patrolling_timer = time.time() # <-- 記錄「巡邏時間」的時間戳
+
+    def handle(self, context, state_data):
+        print("狀態:平台巡邏...")
+
+        # 如果有怪物，切換為戰鬥狀態
+        if state_data.mobs:
+            context.change_state(CombatState())
+            return None, None
+        # if time.time() - self.patrolling_timer > PatrolState.TIMEOUT:
+        #     context.change_state(CombatState())
+        #     return None, None
+        # 沒怪就繼續原本的巡邏動作
+        return context._enable_player_patrol()
+    
+class CombatState(State):
+
+    LOST_TARGET_TIMEOUT = 0.5 # <-- 找不到怪超過幾秒，才切回巡邏
+    def __init__(self):
+        self.lost_target_timer = None # <-- 記錄「開始找不到怪」的時間戳
+
+    def handle(self, context, state_data):
+        print("狀態:進入戰鬥...")
+
+        # 有怪，重置計時器，正常打怪
+        if state_data.mobs:
+            self.lost_target_timer = None
+            return context._fk_that_mob(state_data)
+        
+        # 第一次沒看到怪，記錄時間戳
+        if self.lost_target_timer is None:
+            self.lost_target_timer = time.time()
+            return None, None
+        
+        passed_time = time.time() - self.lost_target_timer
+        print(f"已經{passed_time:.1f} 秒沒有看到怪物了...")
+
+        #  超過預設時間，切回巡邏
+        if passed_time > self.LOST_TARGET_TIMEOUT:
+            print(f"已經{passed_time:.1f} 秒沒有看到怪物了，切回巡邏狀態")
+            context.change_state(PatrolState())
+            return None, None
+        
+        print(f"等待怪物出現中... ({int(passed_time)}秒")
+        return None, None
+    
+class BotState():
+    '''
+    state manager    
+    '''
+    def __init__(self,owner):
+        self.owner = owner
+        self.current_state = PatrolState() #<-- 初始狀態是巡邏
+
+    def change_state(self, new_state):
+        print(f"[狀態切換] {self.current_state.__class__.__name__} -> {new_state.__class__.__name__}")
+        self.current_state = new_state
+
+    #調用
+    def handle(self, state_data):
+        result = self.current_state.handle(self, state_data)
+
+        # 統一防呆:少傳Noe 則多補None 
+        if result is None:
+            return None, None
+        return result
+
+    #====借殼用函式
+    def _enable_player_patrol(self):
+        return self.owner._enable_player_patrol()
+
+    def _fk_that_mob(self, state_data):
+        return self.owner._fk_that_mob(state_data)
